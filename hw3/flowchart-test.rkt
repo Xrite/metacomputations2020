ec275158fd4f66b730fcc04e2d2cf4b45f88f736
Xrite <bukov.anton@gmail.com>
2020-10-19 00:05:04 +0500

    Delete flowchart-test.rkt

--- hw3/flowchart-test.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(require rackunit "flowchart.rkt")
-
-(test-begin
- (let ([state (read-variables '(a b c dd e28 F GGWP228) '(1 2 3 (0 0 0) "e28" F #f))])
-   (check = (eval-expression state '(+ a b)) 3)))
